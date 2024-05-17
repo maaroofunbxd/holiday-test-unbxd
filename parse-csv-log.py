c613bbb61b0e7393c8089fe019e8103ecad4d09a
harsh4723 <harsh4723@rediffmail.com>
2024-05-17 20:12:11 +0530

    test startco site

--- parse-csv-log.py
+++ parse-csv-log.py
@@ -5,13 +5,13 @@ import random
 from random import randrange
 
 
-# csv_file_path = 'log_hodor_gcp_1.csv'
+csv_file_path = 'stratco.csv'
 # csv_file_path_2 = 'log_hodor_gcp_2.csv'
 # csv_file_path_3 = 'log_hodor_gcp_3.csv'
 # csv_file_path_4 = 'log_hodor_gcp_4.csv'
 # csv_file_path_5 = 'log_hodor_gcp_5.csv'
 # text_file_path = "hodor_request_2.txt"
-# df1 = pd.read_csv(csv_file_path)
+df = pd.read_csv(csv_file_path)
 # df2 = pd.read_csv(csv_file_path_2)
 # df3 = pd.read_csv(csv_file_path_3)
 # df4 = pd.read_csv(csv_file_path_4)
@@ -35,69 +35,50 @@ from random import randrange
 
 
 
-# output_entries = []
-# output_entries.append("[")
-# for entry in df['log']:
-#     if "query received" in entry:
-#         # Use regular expression to extract relevant information
-#         match = re.search(r'query received\s+(.*)', entry)
-#         if match:
-#             query_data = json.loads(match.group(1))
-#             query_data['query']['count'] = 36
-#             query_data['query']['deviceType'] = "UNKNOWN"
-#             query_data['query']['norm'] = 40
-#             query_data['query']['platform'] = "netcore"
-#             query_data['query']['algo'] = "vav"
-#             query_data['query']['algoVersion'] = "v2"
-#             query_data['query']['userId'] = stopbed_uids["UID"].sample(n=1).tolist()[0]
-#             query_data['query']['pids'] = []
-#             query_data['query']['sitekey'] = query_data["sitekey"]
-#             if query_data["sitekey"] == "prod-jeromes-us812431587636548":
-#                 query_data["query"]["userId"] = jeromes_uid["user_id"].sample(n=1).tolist()[0]
-#             if query_data["sitekey"] == "prod-miniaturemarket-com811741582229555":
-#                 query_data["query"]["userId"] = random.choice(miniature_uids)
-#             try:
-#                 #print(query_data['query']['filters']['componentFilters_1']['component_filters'][0]['group'])
-#                 query_data['query']['filters']['componentFilters_2']['component_filters'][0]['group'] = 2
-#                 query_data['query']['filters']['componentFilters_1']['component_filters'][0]['group'] = 2
-#             except Exception as e:
-#                 pass
-#             if query_data["query_tag"] == "reranker":
-#                 stdata = json.dumps(query_data)
-#                 output_entries.append(stdata+",")
+output_entries = []
+output_entries.append("[")
+for entry in df['log']:
+    if "query received" in entry:
+        # Use regular expression to extract relevant information
+        match = re.search(r'query received\s+(.*)', entry)
+        if match:
+            query_data = json.loads(match.group(1))
+            stdata = json.dumps(query_data)
+            output_entries.append(stdata+",")
 
-# output_entries[-1] = output_entries[-1][:-1]
-# output_entries.append("]")
+output_entries[-1] = output_entries[-1][:-1]
+output_entries.append("]")
 
-# with open('reranker_request_recsv2_gcp_json_2.json', 'w') as file:
-#     file.write('\n'.join(output_entries))
+with open('stratco_hodor.json', 'w') as file:
+    file.write('\n'.join(output_entries))
 
 
-f = open('hodor_request_gcp_json_2.json')
-data = json.load(f)
-print(data[0]["query_tag"])
+# f = open('hodor_request_gcp_json_2.json')
+# data = json.load(f)
+# print(data[0]["query_tag"])
 
-new_fields = []
-for i in range(80):
-    field_name = f"field_{i+1}"
-    new_fields.append(field_name)
+# new_fields = []
+# for i in range(80):
+#     field_name = f"field_{i+1}"
+#     new_fields.append(field_name)
 
-print(new_fields)
+# print(new_fields)
 
-new_req = []
-for x in data:
-    if x["query_tag"] == "recommender":
-        pro = []
-        l = len(x["query"]["products"])
-        for i in range(100):
-            rr = randrange(l)
-            pro.append(x["query"]["products"][rr])
-        x["query"]["products"] = pro
-        x["query"]["fields"].extend(new_fields)
-        new_req.append(x)
+# new_req = []
+# for x in data:
+#     if x["query_tag"] == "recommender":
+#         pro = []
+#         l = len(x["query"]["products"])
+#         for i in range(100):
+#             rr = randrange(l)
+#             pro.append(x["query"]["products"][rr])
+#         x["query"]["products"] = pro
+#         # x["query"]["fields"].extend(new_fields)
+#         x["query"]["fields"] = ["all_fields"]
+#         new_req.append(x)
        
 
 
 
-with open('hodor_request_gcp_json_recommender_3.json', 'w') as file:
-    json.dump(new_req, file, indent=4)
+# with open('hodor_request_gcp_json_recommender_all_fields_2.json', 'w') as file:
+#     json.dump(new_req, file, indent=4)
--- replay.py
+++ replay.py
@@ -3,11 +3,11 @@ import requests
 
 
 
-with open("mwave_hodor.json", "r") as file:
+with open("stratco_hodor.json", "r") as file:
     request_data = json.load(file)
 
-new_hodor_api = "http://internal-ab9ffcb3f856f40a79c9c85d0fcf4098-37368537.ap-southeast-2.elb.amazonaws.com/sites/ss-unbxd-prod-mwave43601693203163/products/_filter"
-old_hodor_api = "http://hodor.prod.ap-southeast-2.infra/sites/ss-unbxd-prod-mwave43601693203163/products/_filter"
+new_hodor_api = "http://internal-ab9ffcb3f856f40a79c9c85d0fcf4098-37368537.ap-southeast-2.elb.amazonaws.com/sites/prod-stratco4681588782802/products/_filter"
+old_hodor_api = "http://hodor.prod.ap-southeast-2.infra/sites/prod-stratco4681588782802/products/_filter"
 
 def send_request(api_url, request_data):
     headers = {'Content-Type': 'application/json'}
@@ -23,12 +23,17 @@ def compare_responses(response1, response2):
     response2_products_sorted = sorted(response2.get("response", {}).get("products", []), key=lambda x: x["uniqueId"])
     print("res sor",response1_products_sorted)
     print("res sor 2",response2_products_sorted)
+    print("res1 count",response1["response"]["numberOfProducts"])
+    print("res2 count",response2["response"]["numberOfProducts"])
     if response1_products_sorted == response2_products_sorted:
         return True
     else:
         return False
 
 for request in request_data[:1]:
+    if request["query_tag"] != "reranker":
+        continue
+    request["query"]["count"]= 50000
     resp_v2 = send_request(new_hodor_api, request["query"])
     resp_old = send_request(old_hodor_api, request["query"])
     if compare_responses(resp_v2, resp_old):
