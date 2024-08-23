2998fac79d50db63d19cb21bb8e2b8cd6d140619
harsh4723 <harsh4723@rediffmail.com>
2024-08-23 16:22:35 +0530

    add replay

--- replay.py
+++ replay.py
@@ -1,24 +1,26 @@
-import json
 import requests
+import json
 
 
-
-with open("uk_alohas_hodor.json", "r") as file:
+with open("us_angara_hodor.json", "r") as file:
     request_data = json.load(file)
 
-new_hodor_api = "http://internal-a7cd4c58d2f14497a95ab85e73fee6c5-227169144.us-east-1.elb.amazonaws.com/sites/ss-unbxd-prod-alohassandles-shopifyplus43721687326386/products/_filter"
+new_hodor_api = "http://internal-a7cd4c58d2f14497a95ab85e73fee6c5-227169144.us-east-1.elb.amazonaws.com/sites/ss-unbxd-Prod-Angara-US809761688720875/products/_filter"
 old_hodor_api = "http://hodor.prod.eu-west-2.infra/sites/ss-unbxd-prod-alohassandles-shopifyplus43721687326386/products/_filter"
 
 def send_request(api_url, request_data):
     headers = {'Content-Type': 'application/json'}
-    response = requests.post(api_url, json=request_data, headers=headers)
-    res = response.json()
-    if len(res["response"]["products"]) == 0:
-        print("Empty res")
-    if response.status_code == 200:
-        return "OK"
-    return "NOTOK"
+    try:
+        response = requests.post(api_url, json=request_data, headers=headers)
+        res = response.json()
+        if len(res["response"]["products"]) == 0:
+            print("Empty res")
+        if response.status_code == 200:
+            return "OK"
+    except Exception as e:
+        return "NOTOK"
     
+    return "NOTOK"
 
 def compare_responses(response1, response2):
     response1_products_sorted = sorted(response1.get("response", {}).get("products", []), key=lambda x: x["uniqueId"])
@@ -41,9 +43,7 @@ for request in request_data:
         print(resp_v2)
         print(request["query"])
     #resp_old = send_request(old_hodor_api, request["query"])
-    
     # if compare_responses(resp_v2, resp_old):
     #     print("Equal")
     # else:
     #     print("NotEqual")
-
