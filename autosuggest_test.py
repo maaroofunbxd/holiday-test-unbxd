5a3b34add1d57a77c4555358a4e179b28ff0f172
harsh4723 <harsh4723@rediffmail.com>
2024-10-18 13:51:55 +0530

    test

--- autosuggest_test.py
+++ autosuggest_test.py
@@ -7,7 +7,13 @@ csv_file_path = 'camperAsWingman.csv'
 df = pd.read_csv(csv_file_path)
 
 req  = 10
-url = "https://search.unbxd.io" + df['msg'][0]
+# url = "https://search.unbxd.io" + df['msg'][0]
+
+# res = requests.get(url)
+# jres = json.loads(res.content)
+# prod_array =  jres.get('response').get('products')
+# print(prod_array[0].get('autosuggest'))
+
 for i in range(0,req):
     url = "https://search.unbxd.io" + df['msg'][i]
     demo_url = "http://internal-a0484b72775f611ed98b10284fd5d35d-2106550818.eu-west-2.elb.amazonaws.com"+ df['msg'][i]
@@ -18,8 +24,18 @@ for i in range(0,req):
     num_pro = jres.get('response').get('numberOfProducts')
     num_pro_demo = jres_demo.get('response').get('numberOfProducts')
     print(f"num_pro {num_pro} num_pro_demo {num_pro_demo}")
+    prod_array =  jres.get('response').get('products')
+    pro_array_demo = jres_demo.get('response').get('products')
     if num_pro != num_pro_demo :
         print("res different for", url)
+    if len(prod_array) != len(pro_array_demo):
+        print("len prod array is different for url", url)
+        continue
+    for i in range(0, len(prod_array)):
+        if prod_array[i].get("autosuggest") != pro_array_demo[i].get("autosuggest"):
+            print("autosuggest is different for url", url)
+            break
+
 
 
 
