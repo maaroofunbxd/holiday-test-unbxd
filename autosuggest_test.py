fdee0d06aa57796a487d180b4fc6366c0cd39071
harsh4723 <harsh4723@rediffmail.com>
2024-10-24 11:25:29 +0530

    chore: add ss

--- autosuggest_test.py
+++ autosuggest_test.py
@@ -3,28 +3,28 @@ import requests
 import json
 
 
-csv_file_path = 'ukAs.csv'
+csv_file_path = 'apacCFSTas.csv'
 df = pd.read_csv(csv_file_path)
 
 req  = 10
-# url = "https://search.unbxd.io" + df['msg'][0]
-
+# url = "https://search.unbxd.io" + df['ClientRequestURI'][0]
+# print(url)
 # res = requests.get(url)
 # jres = json.loads(res.content)
 # prod_array =  jres.get('response').get('products')
 # print(prod_array[0].get('autosuggest'))
 
 for i in range(0,req):
-    url = "https://search.unbxd.io" + df['msg'][i]
+    url = "https://search.unbxd.io" + df['ClientRequestURI'][i]
     try:
-        demo_url = "http://internal-a0484b72775f611ed98b10284fd5d35d-2106550818.eu-west-2.elb.amazonaws.com"+ df['msg'][i]
+        demo_url = "http://internal-afb22f271b11a11ee8e2006993762d51-1541683563.ap-southeast-1.elb.amazonaws.com"+ df['ClientRequestURI'][i]
         res = requests.get(url)
         demo_res = requests.get(demo_url)
         jres = json.loads(res.content)
-        jres_demo = json.loads(demo_res.content)  
+        jres_demo = json.loads(demo_res.content)
         num_pro = jres.get('response').get('numberOfProducts')
         num_pro_demo = jres_demo.get('response').get('numberOfProducts')
-        print(f"num_pro {num_pro} num_pro_demo {num_pro_demo}")
+        #print(f"num_pro {num_pro} num_pro_demo {num_pro_demo}")
         prod_array =  jres.get('response').get('products')
         pro_array_demo = jres_demo.get('response').get('products')
         if num_pro != num_pro_demo :
@@ -38,7 +38,6 @@ for i in range(0,req):
                 break
     except Exception as e:
         print("exception for url",url)
-        break
 
 print("Finished comparing")
 
