3dc58023494f7dd85ec91d1e4656f6cd8d6723ed
harsh4723 <harsh4723@rediffmail.com>
2024-12-06 11:45:53 +0530

    feat: add gcp as req

--- autosuggest_test.py
+++ autosuggest_test.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 
-csv_file_path = 'apacCFSTas.csv'
+csv_file_path = 'gcpASCF.csv'
 df = pd.read_csv(csv_file_path)
 
 req  = 10
@@ -17,7 +17,7 @@ req  = 10
 for i in range(0,req):
     url = "https://search.unbxd.io" + df['ClientRequestURI'][i]
     try:
-        demo_url = "http://internal-afb22f271b11a11ee8e2006993762d51-1541683563.ap-southeast-1.elb.amazonaws.com"+ df['ClientRequestURI'][i]
+        demo_url = "http://10.110.3.25"+ df['ClientRequestURI'][i]
         res = requests.get(url)
         demo_res = requests.get(demo_url)
         jres = json.loads(res.content)
@@ -28,13 +28,16 @@ for i in range(0,req):
         prod_array =  jres.get('response').get('products')
         pro_array_demo = jres_demo.get('response').get('products')
         if num_pro != num_pro_demo :
-            print("res different for", url)
+            print("num pro different for", url)
         if len(prod_array) != len(pro_array_demo):
             print("len prod array is different for url", url)
             continue
         for i in range(0, len(prod_array)):
             if prod_array[i].get("autosuggest") != pro_array_demo[i].get("autosuggest"):
                 print("autosuggest is different for url", url)
+                break
+            if prod_array[i].get("doctype") != pro_array_demo[i].get("doctype"):
+                print("autosuggest product type is different for url", url)
                 break
     except Exception as e:
         print("exception for url",url)
