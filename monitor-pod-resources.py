c32ee3f610359ed42ed0974683553874db56b12b
Maaroof Abdul Kareem <maaroofabdulkareem@NM027.local>
2025-11-05 13:58:20 +0530

    test container wise monitoring

--- monitor-pod-resources.py
+++ monitor-pod-resources.py
@@ -1,4 +1,4 @@
-#cd holiday-test-unbxd; python3 monitor-pod-resources.py --stats 80 --watch 5  -l "algo in (ranking,embeddings)" --output 20251105-1305test.csv; cd ..
+#cd holiday-test-unbxd; python3 monitor-pod-resources.py --stats 20 --watch 5  -l "app in (reranker-demo)" --output 20251105-1355test.csv; cd ..
 #!/usr/bin/env python3
 """
 Monitor Kubernetes container resources showing current/limit as fractions (per-container tracking)
