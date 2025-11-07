6b4c9b9cfb66d37f248d5bbb1add41321bd27a7d
maaroof <maaroof.kareem@netcoreunbxd.com>
2025-11-07 20:43:48 +0530

    ner logs convertion

--- process_reranker_logs.sh
+++ process_reranker_logs.sh
@@ -1,12 +1,16 @@
 
 # Change to the project directory
 
-# Process all reranker log files
-# find 'reranker-logs' '-name' 'reranker-*.log' '-type' 'f' >>= foreach
-#   str file = echo argv[1]
-#   str basename_file = basename file '.log'
-#   str output_file = echo 'reranker-logs/requests_' basename_file '.jsonl'
-#   python3 'extract_requests.py' '-i' file '-o' output_file
+# Process all log files with given prefix and directory
+# Usage: ./process_reranker_logs.sh <directory> <prefix>
+directory="${1:-reranker-logs}"
+prefix="${2:-reranker}"
+
+find "$directory" -name "${prefix}-*.log" -type f | while read -r file; do
+  basename_file=$(basename "$file" .log)
+  output_file="${directory}/requests_${basename_file}.jsonl"
+  python3 extract_requests.py -i "$file" -o "$output_file"
+done
 
 # AWS S3 commands (commented out)
 # aws s3 ls s3://unbxd-des/rerankerloadtest/
