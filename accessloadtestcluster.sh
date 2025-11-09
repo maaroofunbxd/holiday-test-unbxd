3463cf03c80e79267b65bf0599de23e33a676821
maaroof <maaroof.kareem@netcoreunbxd.com>
2025-11-09 14:41:14 +0530

    fix

--- accessloadtestcluster.sh
+++ accessloadtestcluster.sh
@@ -1,10 +1,10 @@
 # Default command if none provided
-CMD="${1:-sh runloadtest.sh}"
+CMD="${1:-runloadtest.sh}"
 HOST=${2:-http://internal-a33ac7ecf86484bdb9a6a550a45a3f8d-2136975334.us-east-1.elb.amazonaws.com}
 ssh -t ubuntu@ip-10-0-1-231 "
   cd ~/mrf/loadtest/holiday-test-unbxd && 
   git fetch origin >/dev/null 2>&1 && git rebase origin/main >/dev/null 2>&1 &&
-  sudo ${CMD} $HOST
+  sudo sh $CMD $HOST
 "
 
 #HOST=$(ssh ec2-user@usejump.unbxd.io "bash -s" -- < ./accesscluster.sh "./get-service-host.sh reranker")
