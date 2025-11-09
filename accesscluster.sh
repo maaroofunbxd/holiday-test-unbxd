2d788936c2fb83d706233056659828c2e432312b
maaroof <maaroof.kareem@netcoreunbxd.com>
2025-11-09 14:27:02 +0530

    null

--- accesscluster.sh
+++ accesscluster.sh
@@ -7,7 +7,7 @@ CMD="${1:-./clustermonitor.sh}"
 ssh -t ai "
   sudo su - ai-prod-us-east-1-eks -c '
     cd ~/mrf/holiday-test-unbxd &&
-    git fetch origin && git rebase origin/main &&
+    git fetch origin >/dev/null 2>&1 && git rebase origin/main >/dev/null 2>&1 &&
     ${CMD}
   '
 "
--- accessloadtestcluster.sh
+++ accessloadtestcluster.sh
@@ -3,6 +3,6 @@ CMD="${1:-./runloadtest.sh}"
 
 ssh -t ubuntu@ip-10-0-1-231 "
   cd ~/mrf/loadtest/holiday-test-unbxd && 
-  git fetch origin && git rebase origin/main &&
+  git fetch origin >/dev/null 2>&1 && git rebase origin/main >/dev/null 2>&1 &&
   sudo ${CMD}
 "
