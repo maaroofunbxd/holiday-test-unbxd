3a836660da0997bacc3d4a43948715067e851be9
maaroof <maaroof.kareem@netcoreunbxd.com>
2025-11-09 13:59:23 +0530

    extracted apse1 requests

--- log-commands.sh
+++ log-commands.sh
@@ -9,13 +9,14 @@ logfetchstart() {
 
 #steps
 #1. from cluster,upload logs to s3
-ssh ec2-user@usejump.unbxd.io "bash -s" -- < ./accesscluster.sh "'ls reranker-logs'"
+#ssh ec2-user@usejump.unbxd.io "bash -s" -- < ./accesscluster.sh "'sh uploadtos3.sh reranker'"
 sh uploadtos3.sh reranker
 
 #2. ubuntu serverdownload logs from s3
+#ssh ec2-user@usejump.unbxd.io "bash -s" -- < ./accessloadtestcluster.sh "'sh downloadfroms3.sh qcs'"
 sh downloadfroms3.sh qcs
 
 #3. ubuntu server, process logs
-ssh ec2-user@usejump.unbxd.io "bash -s" -- < ./accessloadtestcluster.sh "'./process_logs.sh reranker-logs-ap-southeast-1 reranker reranker'"
+#ssh ec2-user@usejump.unbxd.io "bash -s" -- < ./accessloadtestcluster.sh "'sh ./process_logs.sh reranker-ap-southeast-1-logs reranker reranker'"
 sh ./process_logs.sh ner-logs ner ner
 
