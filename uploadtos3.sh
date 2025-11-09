4cc7b9486d57e8376a0f0eceab5edb72f33b96d3
maaroof <maaroof.kareem@netcoreunbxd.com>
2025-11-09 07:24:36 +0530

    upload fix

--- uploadtos3.sh
+++ uploadtos3.sh
@@ -1,3 +1,3 @@
 service="${1:-ner}"
-aws s3 cp ${service}-logs/. s3://unbxd-des/${service}loadtest/ --recursive --exclude "*" --include "${service}-*.log"
+aws s3 cp ${service}-logs/. s3://unbxd-des/${service}loadtest-ap-southeast-1/ --recursive --exclude "*" --include "${service}-*.log"
 
