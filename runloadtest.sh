7f0622d6cf4c81e0b2eca8b48537fa79b910c0ec
maaroof <maaroof.kareem@netcoreunbxd.com>
2025-11-12 14:45:06 +0530

    useast 50RPS datadog test

--- runloadtest.sh
+++ runloadtest.sh
@@ -15,4 +15,9 @@ k6_run() {
 
 # Then run the load test:
 echo "Running with REGION=$REGION"
-k6_run 100 600s $HOST
+k6_run 50 60s $HOST
+
+#HOST=http://internal-aacb4a1ef44964e9a8d7979f74de2ea7-218368305.ap-southeast-1.elb.amazonaws.com REGION=ap-southeast-1 ./accessloadtestcluster.sh
+#HOST=http://internal-a33ac7ecf86484bdb9a6a550a45a3f8d-2136975334.us-east-1.elb.amazonaws.com REGION=use-1d ./accessloadtestcluster.sh
+#MAX rps
+#cd reranker-use-1d-logs/ ; echo "$(cat *.jsonl | wc -w) / (10*60)" | bc -l
