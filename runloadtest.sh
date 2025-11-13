d8d0fbf056c99c8f4045448758c9e694634b433c
maaroof <maaroof.kareem@netcoreunbxd.com>
2025-11-13 11:51:41 +0530

    testing till 65

--- runloadtest.sh
+++ runloadtest.sh
@@ -31,7 +31,7 @@ echo "Running with REGION=$REGION"
 #k6_run 100 600s $HOST reranker reranker-load-test.js
 
 # Or run stress test to find max RPS:
-stress_test 0 90 8m 2m $HOST reranker reranker-stress-test.js
+stress_test 0 65 8m 2m $HOST reranker reranker-stress-test.js
 
 #HOST=http://internal-aacb4a1ef44964e9a8d7979f74de2ea7-218368305.ap-southeast-1.elb.amazonaws.com REGION=ap-southeast-1 ./accessloadtestcluster.sh
 #HOST=http://internal-a33ac7ecf86484bdb9a6a550a45a3f8d-2136975334.us-east-1.elb.amazonaws.com REGION=use-1d ./accessloadtestcluster.sh
