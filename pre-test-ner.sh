d2452f9d75a3516ca05db0a92429bcea481c53a0
maaroof <maaroof.kareem@netcoreunbxd.com>
2025-11-13 18:18:24 +0530

    toomanyrequests

--- pre-test-ner.sh
+++ pre-test-ner.sh
@@ -1,6 +1,8 @@
 helm get values ner -nsearch -o yaml > ner-prod-values.yaml
 
 cd ~/mrf/entity-extraction/;
+git checkout helminstalldemo
+
 helm template ner-demo ./helm/ner -nsearch\
   -f ner-prod-values.yaml \
   --set service.name=ner-demo \
@@ -41,3 +43,18 @@ helm install ner-demo ./helm/ner -nsearch\
   --set service.annotations."service\.beta\.kubernetes\.io/aws-load-balancer-name"=ner-demo-lb \
   --set service.annotations."service\.beta\.kubernetes\.io/aws-load-balancer-additional-resource-tags"="Environment=demo,Service=ner-demo" \
   --no-hooks
+
+helm upgrade ner-demo ./helm/ner -nsearch \
+  -f ner-prod-values.yaml \
+  --set service.name=ner-demo \
+  --set env=demo \
+  --set app=ner-demo \
+  --set replicaCount=1 \
+  --set service.type=LoadBalancer \
+  --set albus.nats_service=ner-demo \
+  --set deployment.env.ELASTIC_APM_SERVICE_NAME=ner-demo \
+  --set service.annotations."service\.beta\.kubernetes\.io/aws-load-balancer-name"=ner-demo-lb \
+  --set service.annotations."service\.beta\.kubernetes\.io/aws-load-balancer-additional-resource-tags"="Environment=demo,Service=ner-demo" \
+  --no-hooks
+
+#helm uninstall ner-demo -nsearch
