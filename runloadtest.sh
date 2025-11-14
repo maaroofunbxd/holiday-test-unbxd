6093ebcdd49d3d57f51cb5f4b341606adbf5786d
maaroof <maaroof.kareem@netcoreunbxd.com>
2025-11-14 11:51:30 +0530

    NER load test at constant RPS

--- runloadtest.sh
+++ runloadtest.sh
@@ -28,11 +28,10 @@ stress_test() {
 
 # Then run the load test:
 echo "Running with REGION=$REGION"
-#k6_run 100 600s $HOST reranker reranker-load-test.js
+#k6_run 100 600s $HOST ner reranker-load-test.js
 
 # Or run stress test to find max RPS:
-#stress_test 0 65 8m 2m $HOST reranker reranker-stress-test.js
-stress_test 0 100 6m 2m $HOST ner reranker-stress-test.js
+stress_test 0 100 8m 2m $HOST ner reranker-stress-test.js
 
 #HOST=http://internal-aacb4a1ef44964e9a8d7979f74de2ea7-218368305.ap-southeast-1.elb.amazonaws.com REGION=ap-southeast-1 ./accessloadtestcluster.sh
 #HOST=http://internal-a33ac7ecf86484bdb9a6a550a45a3f8d-2136975334.us-east-1.elb.amazonaws.com REGION=use-1d ./accessloadtestcluster.sh
