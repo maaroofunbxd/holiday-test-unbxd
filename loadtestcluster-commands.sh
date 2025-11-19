40c22d96d8f167ca3c38f85fe81e0cf30345f990
maaroof <maaroof.kareem@netcoreunbxd.com>
2025-11-19 14:51:07 +0530

    fix region

--- loadtestcluster-commands.sh
+++ loadtestcluster-commands.sh
@@ -1,4 +1,4 @@
 sh runloadtest.sh $HOST
 bash
 #sh ./downloadfroms3.sh qcs ap-southeast-1
-#sh ./process_logs.sh qcs-ap-southeast-1-logs qcs qcs ap-southeast-1
+#sh ./process_logs.sh qcs-ap-southeast-1prod-logs qcs qcs ap-southeast-1
