ef09a97918a3454fb5b9af0a9879b02160e6960c
maaroof <maaroof.kareem@netcoreunbxd.com>
2025-11-20 00:34:28 +0530

    statsd host should be only use-1d

--- pre-test-qcs.sh
+++ pre-test-qcs.sh
@@ -7,13 +7,13 @@ cd ~/mrf/qcs/;
 
 #git checkout AI-915
 #git fetch origin && git rebase origin/AI-915
-ls -R ./helm
-helm template qcs-demo  ./helm/qcs/ -f ./helm/qcs/values-aws-dev-ap-southeast-1.yaml -nai \
-  --set service.env.SERVICE=qcs-demo \
-  --set app=qcs-demo \
-  --set service.type=LoadBalancer \
-  --set service\.beta\.kubernetes\.io/aws-load-balancer-internal="0.0.0.0/0" \
-  > qcs-demo-manifests.yaml
+# ls -R ./helm
+# helm template qcs-demo  ./helm/qcs/ -f ./helm/qcs/values-aws-dev-ap-southeast-1.yaml -nai \
+#   --set service.env.SERVICE=qcs-demo \
+#   --set app=qcs-demo \
+#   --set service.type=LoadBalancer \
+#   --set service\.beta\.kubernetes\.io/aws-load-balancer-internal="0.0.0.0/0" \
+#   > qcs-demo-manifests.yaml
 
 # helm install qcs-demo ./helm/qcs -nai\
 #   -f qcs-prod-values.yaml \
@@ -52,5 +52,7 @@ helm upgrade qcs-demo  ./helm/qcs/ -f ./helm/qcs/values-aws-dev-ap-southeast-1.y
   --set ELASTIC_APM_SERVER_URL=http://apm.pilot-unbxd.infra:8200 \
   --set service.env.ALBUSCLIENT_ALBUS_HOST=http://configstore.prod.ap-southeast-1.infra \
   --set gunicorn.workerConnections=1000 \
-  --set image.tag=PR_maaroofunbxd_AI-915
+  --set statsd.host=statsd.prod.use-1d.infra
+
+cd ~/mrf/holiday-test-unbxd
 #helm uninstall qcs-demo -nai
