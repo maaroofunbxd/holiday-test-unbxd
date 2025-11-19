67b239adc6f25eccf0bf62bfd8e1e8ce75dde833
maaroof <maaroof.kareem@netcoreunbxd.com>
2025-11-19 19:24:42 +0530

    test start

--- podcluster-commands.sh
+++ podcluster-commands.sh
@@ -1,9 +1,9 @@
 #sudo chown -R $USER:$USER .
-#pip3 install pandas tabulate
-#./clustermonitor.sh qcs-demo 600 ai
-#pip3 uninstall pandas tabulate
+pip3 install pandas tabulate
+./clustermonitor.sh qcs-demo 600 ai
+pip3 uninstall pandas tabulate
 #sh ./pre-test-qcs.sh
-bash
+#bash
 #python3 commonpre-test.py
 #./simple-log-monitor.sh start app=qcs ./qcs-gcp-us-logs
 #kubectl get deploy qcs-demo -n ai -o yaml > demo.yaml
