500b9cf493a66d94f63531b6d0357a8f82fa30ef
maaroof <maaroof.kareem@netcoreunbxd.com>
2025-11-19 16:12:15 +0530

    line by line

--- podcluster-commands.sh
+++ podcluster-commands.sh
@@ -2,7 +2,7 @@
 #pip3 install pandas tabulate
 #./clustermonitor.sh qcs-demo 600 ai
 #pip3 uninstall pandas tabulate
-./pre-test-qcs.sh
+sh ./pre-test-qcs.sh
 #python3 commonpre-test.py
 #./simple-log-monitor.sh start app=qcs ./qcs-gcp-us-logs
 # kubectl get deploy qcs-demo -n ai -o yaml > demo.yaml
