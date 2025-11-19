2a33b0d43356cd2960dd525a0358c73b824954d6
maaroof <maaroof.kareem@netcoreunbxd.com>
2025-11-19 18:03:38 +0530

    arg fix

--- loadtest.sh
+++ loadtest.sh
@@ -5,6 +5,6 @@
 # Terminal 1: Run this to execute load test
 
 # Edit the commands below for your test:
-./run-loadtest-with-host.sh qcs-demo ai ap-southeast-1prod
+./run-loadtest-with-host.sh qcs-demo ap-southeast-1prod ai 
 
 
--- loadtestcluster-commands.sh
+++ loadtestcluster-commands.sh
@@ -1,5 +1,4 @@
-source runloadtest.sh
-. runloadtest.sh $HOST
+./runloadtest.sh $HOST
 bash
 #sh ./downloadfroms3.sh qcs ap-southeast-1
 #sh ./process_logs.sh qcs-ap-southeast-1prod-logs qcs qcs ap-southeast-1
