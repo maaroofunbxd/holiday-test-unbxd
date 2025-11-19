c64965196a17b981a0817143a744de1275d160a3
maaroof <maaroof.kareem@netcoreunbxd.com>
2025-11-19 06:06:51 +0530

    push

--- podcluster-commands.sh
+++ podcluster-commands.sh
@@ -1,6 +1,6 @@
 #./clustermonitor.sh qcs-demo 600 search
-kubectl get deploy qcs-demo -n search -o yaml > demo.yaml
-kubectl get deploy qcs -n search -o yaml > prod.yaml
+kubectl get deploy qcs-demo -n ai -o yaml > demo.yaml
+kubectl get deploy qcs -n ai -o yaml > prod.yaml
 diff demo.yaml prod.yaml
 bash
 #python3 commonpre-test.py
